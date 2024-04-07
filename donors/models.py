82f243ddb277f839832ee0442789466cf7c88dad
pranvera24 <pranvera.halili243@gmail.com>
2024-04-07 15:02:47 +0200

    second commit

--- donors/models.py
+++ donors/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Donor(models.Model):
-    name = models.CharField(max_length=100, null=True)  # Allow null values for user input
-    surname = models.CharField(max_length=100, null=True)  # Allow null values for user input
-    age = models.PositiveIntegerField(null=True)  # Allow null values for user input
-    email = models.EmailField(null=True)  # Allow null values for user input
-    phone_number = models.CharField(max_length=15, null=True)  # Allow null values for user input
+    name = models.CharField(max_length=100, null=True)  
+    surname = models.CharField(max_length=100, null=True)  
+    age = models.PositiveIntegerField(null=True)  
+    email = models.EmailField(null=True)  
+    phone_number = models.CharField(max_length=15, null=True)  
     blood_group = models.CharField(max_length=5, choices=[('A+', 'A+'), ('A-', 'A-'), ('B+', 'B+'), ('B-', 'B-'), ('AB+', 'AB+'), ('AB-', 'AB-'), ('O+', 'O+'), ('O-', 'O-')], null=True)  # Allow null values for user input
     HEALTH_CHOICES = [
         ('None', 'None'),
@@ -15,8 +15,8 @@ class Donor(models.Model):
         ('Heart Disease', 'Heart Disease'),
         ('Other', 'Other')
     ]
-    health_history = models.CharField(max_length=50, choices=HEALTH_CHOICES, null=True)  # Allow null values for user input
-    last_donation_date = models.DateField(null=True, blank=True)  # Allow null values for user input
+    health_history = models.CharField(max_length=50, choices=HEALTH_CHOICES, null=True)  
+    last_donation_date = models.DateField(null=True, blank=True)  
 
     def __str__(self):
         return self.name
